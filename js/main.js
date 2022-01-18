613ebcb22be814053fa2a953a3aa9ebd0c728ccd
filip.joksovic@student.um.si <filip.joksovic@student.um.si>
2022-01-18 04:59:29 +0100

    Unnecessary comments on expressions page removed

--- js/main.js
+++ js/main.js
@@ -14,36 +14,27 @@ function evaluateFromString(str) {
 }
 
 function clearExpression() {
-    console.log('clearing')
     $("#expression").val("")
     $("#expression").change()
 }
 
 function removeLastChar() {
     let newVal = $("#expression").val().substring(0, $("#expression").val().length - 1)
-    console.log(newVal)
     $("#expression").val(newVal)
     $("#expression").change()
 }
-console.log($("#fUp"))
 $("#fUp").change(function () {
     let file = document.getElementById("fUp")
-    console.log(file)
-    console.log(file.files)
-    console.log(file.files[0])
     let selectedFile = file.files[0]
     var fr = new FileReader();
     let textContent;
     fr.onload = function () {
         textContent = fr.result
-        console.log(textContent.split("\n"))
         equationsArray(textContent.split("\n"))
         setTextContent(textContent, fr.result)
         calculateFromFile()
     }
     fr.readAsText(file.files[0])
-    console.log(textContent)
-    // console.log(fr.readAsText(file.files[0]));
 })
 equations = []
 
@@ -51,7 +42,6 @@ function equationsArray(arr) {
     for (let element of arr) {
         equations.push(element.replace("=", "").replace("\r", ""))
     }
-    console.log(equations)
 }
 
 function calculateFromFile() {
