93201361a8d7e926ca5177ff48a91f973d733203
filip.joksovic@student.um.si <filip.joksovic@student.um.si>
2022-01-18 04:58:53 +0100

    Math expressions from a file now work

--- js/main.js
+++ js/main.js
@@ -1,43 +1,65 @@
-
-function addToExpression(val){
+function addToExpression(val) {
     document.getElementById("expression").value = document.getElementById("expression").value + val
 }
-function evaluateExpression(){
+
+function evaluateExpression() {
     expression = document.getElementById("expression").value
     document.getElementById("result").value = expression + " = " + math.evaluate(expression)
     clearExpression()
-}  
-function clearExpression(){
+}
+
+function evaluateFromString(str) {
+    document.getElementById("result").value += str + " = " + math.evaluate(str) + "\n"
+    clearExpression()
+}
+
+function clearExpression() {
     console.log('clearing')
     $("#expression").val("")
     $("#expression").change()
 }
-function removeLastChar(){
-    let newVal = $("#expression").val().substring(0,$("#expression").val().length - 1)
+
+function removeLastChar() {
+    let newVal = $("#expression").val().substring(0, $("#expression").val().length - 1)
     console.log(newVal)
     $("#expression").val(newVal)
     $("#expression").change()
 }
 console.log($("#fUp"))
-$("#fUp").change(function(){
+$("#fUp").change(function () {
     let file = document.getElementById("fUp")
     console.log(file)
     console.log(file.files)
     console.log(file.files[0])
     let selectedFile = file.files[0]
-    $.ajax({
-        url:'/upload',
-        type:'POST',
-        contentType:"application/json",
-        data:{
-            'file':selectedFile
-        },
-        success:function(response){
-            console.log(response)
-        },
-        error:function(response){
-            console.log(response)
+    var fr = new FileReader();
+    let textContent;
+    fr.onload = function () {
+        textContent = fr.result
+        console.log(textContent.split("\n"))
+        equationsArray(textContent.split("\n"))
+        setTextContent(textContent, fr.result)
+        calculateFromFile()
+    }
+    fr.readAsText(file.files[0])
+    console.log(textContent)
+    // console.log(fr.readAsText(file.files[0]));
+})
+equations = []
 
-        }
-    })
-}) 
+function equationsArray(arr) {
+    for (let element of arr) {
+        equations.push(element.replace("=", "").replace("\r", ""))
+    }
+    console.log(equations)
+}
+
+function calculateFromFile() {
+    for (let equation of equations) {
+        evaluateFromString(equation)
+    }
+}
+
+function setTextContent(tc, content) {
+    tc = content
+}
